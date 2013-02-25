bd1cb5b8512d4b17aca8642813f049e205dc0112
Max F. Albrecht <1@178.is>
2013-02-25 19:20:32 +0100

    refactor proxy from simple table to within own http server
    - this means we can handle ALL incoming requests
    - log them
    - redirect to listing if not found

--- proxy.js
+++ proxy.js
@@ -16,9 +16,9 @@ var net         = require('net'),
     prox        = require('flatiron').app,
     mu          = require('mu2'),
     util        = require('util'),
-    proxies    = JSON.parse(fs.readFileSync(path.join(__dirname, 'proxies.json')))["proxies"], // the proxies.json file
-    routerData = {},
-    routesList = {};
+    proxies    = JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'proxies.json')))["proxies"], // the proxies.json file
+    routingTable = {},
+    routingTable = {};
 
 
 prox.config.argv(); // conf source: arguments is most important
@@ -26,32 +26,43 @@ prox.config.env();  // then env vars
 // lastly, our config.json file
 prox.config.file({ file: path.join(__dirname, 'config', 'config.json') }); 
 
-
-console.log(proxies);
+// build the routing table
+var routingTable = {};
+routingTable.router = {};
 
 for (var i=0; i<proxies.length; i++)
 {
-  var hostname = proxies[i].hostname;
-  var remote = proxies[i].remote;
-  routesList[hostname] = remote;
-  console.info("routesList: " + JSON.stringify(routesList));
+  routingTable.router[proxies[i].hostname] = proxies[i].remote;
 };
 
-routerData.router = routesList;
-console.info("routerData: " + JSON.stringify(routerData))
+console.info("routingTable: " + JSON.stringify(routingTable))
 
-// prox.init();
 
-//
-// Http Proxy Server with Proxy Table
-//
-httpProxy.createServer(routerData).listen(prox.config.get('proxy-port'));
-console.info('Proxy Server running at Port ' + prox.config.get('proxy-port'));
+// Create a new instance of HttProxy to use in your server
+var proxy = new httpProxy.RoutingProxy();
 
-//
+// Create a regular http server and proxy its handler
+http.createServer(function (req, res) {  
+  console.log("req for: ", req.headers.host);
+  
+  var cnf = {
+    "host": "localhost",
+    "port": prox.config.get('listing-port') || 8888
+  };
+  
+  if (routingTable.router[req.headers.host]) {
+    cnf.host = req.headers.host;
+    var remote = routingTable.router[req.headers.host];
+    cnf.port = remote.substring(remote.indexOf(':') + 1);
+  }
+  console.log(cnf);
+  proxy.proxyRequest (req, res, cnf);    
+
+// after creation, server listens on configured port
+}).listen(prox.config.get('proxy-port'));
+
+
 // Built-in HTTP Server for Service listing
-//
-
 http.createServer(function (req, res) {
   var stream = mu.compileAndRender('index.mustache', {
     "server_name": prox.config.get('name'),
